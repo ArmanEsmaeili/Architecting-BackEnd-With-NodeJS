08b6b9b41a9c76bbf01d7976133f1010d867bd33
Arman Esmaeili <4rmanesmaeili@gmail.com>
2025-01-27 10:53:42 +0330

    implementing Error first callback pattern

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,8 +1,23 @@
 const fs = require('fs');
+// Blocking, synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textIn);
+// const textOut = `this is the text i wanna add: ${textIn}.\ncreated on ${Date.now()}`
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File Written seccusfully!');
 
-const textOut = `this is the text i wanna add: ${textIn}.\ncreated on ${Date.now()}`
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log('File Written seccusfully!');
+// Non-Blocking - Asymchronous way
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) =>{
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) =>{
+        console.log(data2);
+        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) =>{
+            console.log(data3);
+        
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+              console.log('your file has been written😜');
+            })
+        });
+    });
+});
+console.log("yeeaaaaaah");
